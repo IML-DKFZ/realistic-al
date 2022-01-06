ce81bcee3d54359285bbe70cd4b2eba41828e0b6
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-01-06 14:24:30 +0100

    staging chaotic state

---
+++ src/data/datasets/README.md
@@ -0,0 +1,5 @@
+## Custom Datasets
+List of Custom Datasets from:
+https://github.com/linusericsson/ssl-transfer
+
+Follow Instructions for Installation of Datasets.
---
+++ src/data/datasets/__init__.py
@@ -0,0 +1,13 @@
+from .dtd import DTD
+from .pets import Pets
+from .cars import Cars
+from .food import Food
+from .sun397 import SUN397
+from .voc2007 import VOC2007
+from .flowers import Flowers
+from .aircraft import Aircraft
+from .caltech101 import Caltech101
+
+# Currently ISIC, ChestX etc are not nicely downloadable
+# from .chestx import
+# from.isic import
