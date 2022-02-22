9592eb6e783940894c5097cbd90185298e133b52
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-02-22 11:49:17 +0100

    add new cifar100 sweep

---
+++ launchers/test_cifar100_fixmatch.py
@@ -0,0 +1,57 @@
+from argparse import ArgumentParser
+from launcher import ExperimentLauncher
+
+# Add Transformations from Randaugment and Changing of Learning Rates
+
+config_dict = {
+    "model": ["resnet_fixmatch", "wideresnet-cifar10"],
+    # "model": "wideresnet-cifar10",
+    "data": "cifar100",
+    "active": "standard",
+    "optim": "sgd_fixmatch",
+}
+
+hparam_dict = {
+    "active.num_labelled": [400],
+    "model.dropout_p": [0],
+    "model.learning_rate": 0.03,  # is more stable than 0.1!
+    "model.small_head": [True],
+    "model.use_ema": [True],
+    "model.load_pretrained": None,
+    "trainer.max_epochs": 10000,
+    "trainer.seed": [12345],  # , 12346, 12347],
+    "data.transform_train": [
+        "cifar_basic",
+        "cifar_randaugment",
+    ],
+    "sem_sl.eman": [False],
+}
+
+naming_conv = (
+    "test_fixmatch_{data}_lab-{active.num_labelled}_{model}_ep-{trainer.max_epochs}"
+)
+# naming_conv = "sweep_fixmatch_{data}_{model}_{trainer.max_epochs}_{active.num_labelled}"  # {model}"
+path_to_ex_file = "src/run_training_fixmatch.py"
+
+joint_iteration = None
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(add_help=False)
+    ExperimentLauncher.add_argparse_args(parser)
+    launcher_args = parser.parse_args()
+
+    config_dict, hparam_dict = ExperimentLauncher.modify_params_for_args(
+        launcher_args, config_dict, hparam_dict
+    )
+
+    launcher = ExperimentLauncher(
+        config_dict,
+        hparam_dict,
+        launcher_args,
+        naming_conv,
+        path_to_ex_file,
+        joint_iteration=joint_iteration,
+    )
+
+    launcher.launch_runs()
