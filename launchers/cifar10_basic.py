5bad9d4f1736564ae33487a7cc42bbdd1811170e
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-04-19 14:10:49 +0200

    update cifar10-experiments

--- launchers/cifar10_basic.py
+++ launchers/cifar10_basic.py
@@ -3,9 +3,20 @@ from launcher import ExperimentLauncher
 
 config_dict = {
     "model": "resnet",
-    "query": ["random", "entropy", "kcentergreedy", "bald"],
+    "query": [
+        "random",
+        "entropy",
+        "kcentergreedy",
+        "bald",
+        "variationratios",
+        # "batchbald",
+    ],
     "data": ["cifar10"],  # , "cifar100"],
-    "active": ["standard"],  # did not run! "standard_250", "cifar10_low_data"
+    "active": [
+        "standard",
+        "standard_250",
+        "cifar10_low_data",
+    ],  # did not run! "standard_250", "cifar10_low_data"
     "optim": ["sgd"],
 }
 
@@ -15,9 +26,14 @@ hparam_dict = {
     "model.dropout_p": [0, 0.5],
     "model.learning_rate": [0.01],
     "model.use_ema": False,
+    "data.transform_train": [
+        "cifar_basic",
+        "cifar_randaugment",
+    ],
 }
 naming_conv = (
-    "active_basic_{data}_set-{active}_{model}_acq-{query}_ep-{trainer.max_epochs}"
+    "{data}/active-{active}/model-{model}_drop-{model.dropout_p}_aug-{data.transform_train}_acq-{query}_ep-{trainer.max_epochs}"
+    # "active_basic_{data}_set-{active}_{model}_acq-{query}_ep-{trainer.max_epochs}"
 )
 
 joint_iteration = None
