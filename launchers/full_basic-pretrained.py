255bbfa5a04ea1691136ad2e3e43b8b2d3af990b
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-05-19 13:33:48 +0200

    add comparison runs with 100% data

---
+++ launchers/full_basic-pretrained.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+from launcher import ExperimentLauncher
+
+config_dict = {
+    "model": "resnet",
+    "query": [
+        "random",
+    ],
+    # "data": ["cifar10"],  # , "cifar100"],
+    "data": ["cifar100"],  # , "cifar100"],
+    "active": [
+        "full_data",
+    ],
+    "optim": ["sgd"],
+}
+
+hparam_dict = {
+    "trainer.seed": [12345, 12346, 12347],
+    "trainer.max_epochs": 80,  # Think about this before commiting (or sweep!)
+    "model.dropout_p": [0, 0.5],
+    "model.learning_rate": [0.001],
+    "model.freeze_encoder": [False],  # possibly add True
+    # "model.finetune": [True],
+    "model.use_ema": False,
+    "model.load_pretrained": True,
+    "data.transform_train": [
+        "cifar_basic",
+        "cifar_randaugment",
+    ],
+    "model.small_head": [False],
+    "trainer.precision": 32,
+}
+naming_conv = (
+    "{data}/{active}/basic_model-{model}_drop-{model.dropout_p}_aug-{data.transform_train}_acq-{query}_ep-{trainer.max_epochs}"
+    # "active_basic_{data}_set-{active}_{model}_acq-{query}_ep-{trainer.max_epochs}"
+)
+
+joint_iteration = ["model.load_pretrained", "trainer.seed"]
+
+path_to_ex_file = "src/run_training.py"
+
+if __name__ == "__main__":
+    parser = ArgumentParser(add_help=False)
+    ExperimentLauncher.add_argparse_args(parser)
+    launcher_args = parser.parse_args()
+
+    config_dict, hparam_dict = ExperimentLauncher.modify_params_for_args(
+        launcher_args, config_dict, hparam_dict
+    )
+
+    launcher = ExperimentLauncher(
+        config_dict,
+        hparam_dict,
+        launcher_args,
+        naming_conv,
+        path_to_ex_file,
+        joint_iteration=joint_iteration,
+    )
+
+    launcher.launch_runs()
---
+++ launchers/full_basic.py
@@ -0,0 +1,54 @@
+from argparse import ArgumentParser
+from launcher import ExperimentLauncher
+
+config_dict = {
+    "model": "resnet",
+    "query": [
+        "random",
+    ],
+    "data": ["cifar10", "cifar100"],
+    "active": [
+        "full_data",
+    ],
+    "optim": ["sgd"],
+}
+
+hparam_dict = {
+    "trainer.seed": [12345, 12346, 12347],
+    "trainer.max_epochs": 200,
+    "model.dropout_p": [0, 0.5],
+    "model.learning_rate": [0.01],
+    "model.use_ema": False,
+    "data.transform_train": [
+        "cifar_basic",
+        "cifar_randaugment",
+    ],
+}
+naming_conv = (
+    "{data}/{active}/basic_model-{model}_drop-{model.dropout_p}_aug-{data.transform_train}_acq-{query}_ep-{trainer.max_epochs}"
+    # "active_basic_{data}_set-{active}_{model}_acq-{query}_ep-{trainer.max_epochs}"
+)
+
+joint_iteration = None
+
+path_to_ex_file = "src/run_training.py"
+
+if __name__ == "__main__":
+    parser = ArgumentParser(add_help=False)
+    ExperimentLauncher.add_argparse_args(parser)
+    launcher_args = parser.parse_args()
+
+    config_dict, hparam_dict = ExperimentLauncher.modify_params_for_args(
+        launcher_args, config_dict, hparam_dict
+    )
+
+    launcher = ExperimentLauncher(
+        config_dict,
+        hparam_dict,
+        launcher_args,
+        naming_conv,
+        path_to_ex_file,
+        joint_iteration=joint_iteration,
+    )
+
+    launcher.launch_runs()
