6513033fb52433b2d309141deff581d6a03dedf1
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-08-25 15:38:32 +0200

    update isic2019 sweeps

--- launchers/sweep_isic2019_fixmatch.py
+++ launchers/sweep_isic2019_fixmatch.py
@@ -2,17 +2,15 @@ from argparse import ArgumentParser
 from launcher import ExperimentLauncher
 
 config_dict = {
-    "model": ["resnet"],
+    "model": "resnet_fixmatch",
     "data": "isic2019",
-    "optim": ["sgd"],
+    "optim": ["sgd_fixmatch"],
 }
 
 hparam_dict = {
     "trainer.run_test": False,
     "active.num_labelled": [40, 200, 800],
     "data.val_size": [200, 1000, None],
-    # "active.num_labelled": [200, 800],
-    # "data.val_size": [1000, None],
     "model.dropout_p": [0],
     "model.learning_rate": [0.1, 0.01],
     "model.weight_decay": [5e-3, 5e-4],
@@ -21,14 +19,14 @@ hparam_dict = {
     "model.weighted_loss": True,
     "trainer.max_epochs": 200,
     "trainer.seed": [12345, 12346, 12347],
-    "data.transform_train": ["isic_train", "isic_randaugment"],
+    "data.transform_train": ["isic_train"],
 }
 
 joint_iteration = [["active.num_labelled", "data.val_size"]]
 
 naming_conv = "sweep/{data}/basic_lab-{active.num_labelled}_{model}_ep-{trainer.max_epochs}_drop-{model.dropout_p}_lr-{model.learning_rate}_wd-{model.weight_decay}_opt-{optim}_trafo-{data.transform_train}"
 
-path_to_ex_file = "src/run_training.py"
+path_to_ex_file = "src/run_training_fixmatch.py"
 
 
 if __name__ == "__main__":
