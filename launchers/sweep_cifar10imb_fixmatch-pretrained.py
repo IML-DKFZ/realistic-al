ec3bfad9d93446ddc83e562e516b0946961023c1
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-09-01 11:01:28 +0200

    update sweeps

--- launchers/sweep_cifar10imb_fixmatch-pretrained.py
+++ launchers/sweep_cifar10imb_fixmatch-pretrained.py
@@ -4,32 +4,47 @@ from launcher import ExperimentLauncher
 # Add Transformations from Randaugment and Changing of Learning Rates
 
 config_dict = {
-    "model": "wideresnet-cifar100",
-    "data": "cifar100",
+    "model": ["resnet_fixmatch"],
+    # "model": "wideresnet-cifar10",
+    "data": "cifar10_imb",
     "active": "standard",
     "optim": "sgd_fixmatch",
 }
 
+load_pretrained = [
+    "SSL/cifar10_imb/cifar_resnet18/2022-07-15_11-31-31-278397/checkpoints/last.ckpt",  # seed = 12345
+    "SSL/cifar10_imb/cifar_resnet18/2022-07-15_11-31-34-631836/checkpoints/last.ckpt",  # seed = 12346
+    "SSL/cifar10_imb/cifar_resnet18/2022-07-15_11-31-34-632366/checkpoints/last.ckpt",  # seed = 12347
+]
+
 hparam_dict = {
-    "active.num_labelled": [500, 1000],  # , 1000, 5000],
-    "data.val_size": [2500, None],
+    "trainer.run_test": False,
+    "model.weighted_loss": True,
+    "active.num_labelled": [50, 250],
+    "data.val_size": [50 * 5, 250 * 5],
     "model.dropout_p": [0],
+    "model.weight_decay": [5e-3, 1e-3, 5e-4],
     "model.learning_rate": 0.03,  # is more stable than 0.1!
     "model.small_head": [True],
-    "model.weight_decay": [1e-3],
     "model.use_ema": [False],
     "trainer.max_epochs": 200,
     "trainer.seed": [12345, 12346, 12347],
     "data.transform_train": ["cifar_basic",],
     "sem_sl.eman": [False],
-    "trainer.precision": 16,
+    "model.load_pretrained": load_pretrained,
 }
 
-naming_conv = "sweep/{data}/fixmatch_wideresnet_lab-{active.num_labelled}_model-{model}_ep-{trainer.max_epochs}_wd-{model.weight_decay}"
+naming_conv = (
+    "sweep/{data}/fixmatch_basic_lab-{active.num_labelled}_{model}_ep-{trainer.max_epochs}_wd-{model.weight_decay}"
+    # "sweep_fixmatch_{data}_lab-{active.num_labelled}_{model}_ep-{trainer.max_epochs}"
+)
 # naming_conv = "sweep_fixmatch_{data}_{model}_{trainer.max_epochs}_{active.num_labelled}"  # {model}"
 path_to_ex_file = "src/run_training_fixmatch.py"
 
-joint_iteration = ["active.num_labelled", "data.val_size"]
+joint_iteration = [
+    ["model.load_pretrained", "trainer.seed"],
+    ["active.num_labelled", "data.val_size"],
+]
 
 
 if __name__ == "__main__":
@@ -49,7 +64,5 @@ if __name__ == "__main__":
         path_to_ex_file,
         joint_iteration=joint_iteration,
     )
-    if launcher_args.cluster:
-        launcher.ex_call = "cluster_run --launcher run_active_2gpu.sh"
 
     launcher.launch_runs()
