95a03f100c6696a8233b1ca2806cf9884c1b92f3
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-09-06 10:50:27 +0200

    try fixing contiguous issue for SimCLR

--- skripts/train_simclr.py
+++ skripts/train_simclr.py
@@ -57,6 +57,45 @@ class SimCLR_algo(SimCLR):
         else:
             raise NotImplementedError
 
+    def nt_xent_loss(self, out_1, out_2, temperature, eps=1e-6):
+        """
+        assume out_1 and out_2 are normalized
+        out_1: [batch_size, dim]
+        out_2: [batch_size, dim]
+        """
+        # gather representations in case of distributed training
+        # out_1_dist: [batch_size * world_size, dim]
+        # out_2_dist: [batch_size * world_size, dim]
+        if torch.distributed.is_available() and torch.distributed.is_initialized():
+            out_1_dist = SyncFunction.apply(out_1)
+            out_2_dist = SyncFunction.apply(out_2)
+        else:
+            out_1_dist = out_1
+            out_2_dist = out_2
+
+        # out: [2 * batch_size, dim]
+        # out_dist: [2 * batch_size * world_size, dim]
+        out = torch.cat([out_1, out_2], dim=0)
+        out_dist = torch.cat([out_1_dist, out_2_dist], dim=0)
+
+        # cov and sim: [2 * batch_size, 2 * batch_size * world_size]
+        # neg: [2 * batch_size]
+        cov = torch.mm(out.contiguous(), out_dist.t().contiguous())
+        sim = torch.exp(cov / temperature)
+        neg = sim.sum(dim=-1)
+
+        # from each row, subtract e^(1/temp) to remove similarity measure for x1.x1
+        row_sub = Tensor(neg.shape).fill_(math.e ** (1 / temperature)).to(neg.device)
+        neg = torch.clamp(neg - row_sub, min=eps)  # clamp for numerical stability
+
+        # Positive similarity, pos becomes [2 * batch_size]
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+        pos = torch.cat([pos, pos], dim=0)
+
+        loss = -torch.log(pos / (neg + eps)).mean()
+
+        return loss
+
 
 class SSLOnlineEvaluatorDDP(SSLOnlineEvaluator):
     def on_pretrain_routine_start(
