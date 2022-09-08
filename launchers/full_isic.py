e8b335da8577a2b16a9e65d9b4ffdea61133df38
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-09-08 10:02:33 +0200

    update and add launches

---
+++ launchers/full_isic.py
@@ -0,0 +1,52 @@
+from argparse import ArgumentParser
+from launcher import ExperimentLauncher
+
+config_dict = {
+    "model": "resnet",
+    "query": ["random",],
+    "data": ["isic2019"],
+    "active": ["full_data",],
+    "optim": ["sgd"],
+}
+
+hparam_dict = {
+    "trainer.seed": [12345, 12346, 12347],
+    "trainer.max_epochs": 200,
+    "model.dropout_p": [0],
+    "trainer.precision": 32,
+    "model.learning_rate": [0.1, 0.01],
+    "model.weight_decay": [5e-3, 5e-4],
+    "model.use_ema": False,
+    "model.small_head": [True],
+    "model.weighted_loss": True,
+    "trainer.max_epochs": 200,
+    "trainer.seed": [12345, 12346, 12347],
+    "data.transform_train": ["isic_randaugment", "isic_train"],
+    "trainer.deterministic": True,
+    "trainer.num_workers": 12,
+}
+naming_conv = "{data}/{active}/basic_model-{model}_drop-{model.dropout_p}_aug-{data.transform_train}_wd-{model.weight_decay}_lr-{model.learning_rate}"
+
+joint_iteration = None
+
+path_to_ex_file = "src/run_training.py"
+
+if __name__ == "__main__":
+    parser = ArgumentParser(add_help=False)
+    ExperimentLauncher.add_argparse_args(parser)
+    launcher_args = parser.parse_args()
+
+    config_dict, hparam_dict = ExperimentLauncher.modify_params_for_args(
+        launcher_args, config_dict, hparam_dict
+    )
+
+    launcher = ExperimentLauncher(
+        config_dict,
+        hparam_dict,
+        launcher_args,
+        naming_conv,
+        path_to_ex_file,
+        joint_iteration=joint_iteration,
+    )
+
+    launcher.launch_runs()
