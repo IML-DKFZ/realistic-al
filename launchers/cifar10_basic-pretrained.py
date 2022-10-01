04467b32b545e0f7d638fb41266f91b42afad21e
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2022-10-01 19:00:14 +0200

    cifar10 PT-Batchbald final

--- launchers/cifar10_basic-pretrained.py
+++ launchers/cifar10_basic-pretrained.py
@@ -8,7 +8,7 @@ config_dict = {
         # "random",
         # "entropy",
         # "kcentergreedy",
-        "bald",
+        # "bald",
         # "variationratios",
         "batchbald",
     ],
@@ -33,23 +33,26 @@ load_pretrained = [
 hparam_dict = {
     # "data.val_size": [250, 2500, None],
     "data.val_size": [250],
-    "trainer.batch_size": 1024,
     "trainer.seed": [12345, 12346, 12347],
-    "trainer.max_epochs": 80,  # Think about this before commiting (or sweep!)
+    "trainer.max_epochs": 80,
     # "model.dropout_p": [0, 0, 0, 0.5],
-    "model.dropout_p": [0.5, 0.5],
+    # "model.dropout_p": [0.5, 0.5],
+    "model.dropout_p": [0.5],
     "model.learning_rate": [0.001],
     "model.freeze_encoder": [False],  # possibly add True
     # "model.finetune": [True],
     "model.use_ema": False,
     "model.load_pretrained": True,
-    "data.transform_train": "cifar_rand",
+    "data.transform_train": "cifar_randaugment",
     # experiment with big head and frozen encoder
     # "model.freeze_encoder": True,
     "model.small_head": [False],
-    # "trainer.precision": 32,
-    "trainer.precision": 16,
+    "trainer.precision": 32,
     "trainer.deterministic": True,
+    # possible Future exp
+    # "trainer.precision": 16,
+    # "data.transform_train": "cifar_randaugmentMC",
+    # "trainer.batch_size": 1024,
 }
 
 naming_conv = "{data}/active-{active}/basic-pretrained_model-{model}_drop-{model.dropout_p}_aug-{data.transform_train}_acq-{query}_ep-{trainer.max_epochs}_freeze-{model.freeze_encoder}_smallhead-{model.small_head}"
